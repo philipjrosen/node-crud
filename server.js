e28eb6dd634659933c6325ec531d928f530f39fa
Philip Rosen <philipjrosen@gmail.com>
2017-02-06 10:06:30 -0800

    MongoDB Start/Environment Variables

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-// grab our dependencies
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-const expressLayouts = require('express-ejs-layouts');
+// load environment variables
+require('dotenv').config();
 
-// configure our application
+// grab our dependencies
+const express    = require('express'),
+  app            = express(),
+  port           = process.env.PORT || 8080,
+  expressLayouts = require('express-ejs-layouts'),
+  mongoose       = require('mongoose');
+
+// configure our application ===================
 // tell express where to look for static assets
 app.use(express.static(__dirname + '/public'));
 
@@ -12,10 +16,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
-// set the routes
-app.use(require('./app/routes'))
+// connect to our database
+mongoose.connect(process.env.DB_URI);
 
-// start our server
+// set the routes =============================
+app.use(require('./app/routes'));
+
+// start our server ===========================
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
